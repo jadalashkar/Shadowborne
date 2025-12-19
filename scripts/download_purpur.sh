268d299f6445670cb637b8e61e3fd3bbccc4398d
jadalashkar <jadalashkario.333@gmail.com>
2025-12-19 21:33:11 +0300

    Update download_purpur.sh

--- scripts/download_purpur.sh
+++ scripts/download_purpur.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+set -e  # أي خطأ يوقف السكربت فورًا
 
-# هذا سطر تعليق فقط
-mkdir -p server  # هذا السطر فيه أمر + تعليق، فقط الجزء قبل # ينفذ
+# إنشاء مجلد server إذا مش موجود
+mkdir -p server
+
+# تنزيل Purpur.jar
 curl -L -o server/purpur.jar https://api.purpurmc.org/v2/purpur/1.20.4/latest/download
