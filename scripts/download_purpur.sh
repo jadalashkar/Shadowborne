47e3a230d5304d23320da1c363df6216f68ecf4a
jadalashkar <jadalashkario.333@gmail.com>
2025-12-21 17:03:26 +0300

    Delete scripts directory

--- scripts/download_purpur.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  # أي خطأ يوقف السكربت فورًا
-
-# إنشاء مجلد server إذا مش موجود
-mkdir -p server
-
-# تنزيل Purpur.jar
-curl -L -o server/purpur.jar https://api.purpurmc.org/v2/purpur/1.20.4/latest/download
